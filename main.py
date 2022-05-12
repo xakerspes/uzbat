61fff8331a2eeff04c891fd6843541c57f3d642b
xakerspes <38161328+xakerspes@users.noreply.github.com>
2022-05-12 13:49:51 +0500

    new 12.05.22

--- main.py
+++ main.py
@@ -1,59 +1,90 @@
 import pandas as pd
 import datetime
 import numpy as np
-class A:
-    def __init__(self, freq: str):
-        self.freq = freq
-        a = pd.read_excel('workbook.xlsx')
-        self.df = pd.DataFrame(a)
-        self.result = None
- 
-    def raznitsa(self):
-        
-        self.df['Time'] = pd.to_datetime(self.df['Time'])
-        df = self.df.groupby(pd.Grouper(key="Time", freq=self.freq)).agg(['min', 'max'])
-        for i in range(0,len(df.columns)-1,2):
-            df[df.columns[i][0]+"_result"] =  df[df.columns[i+1]] - df[df.columns[i]]
-        df = df.dropna()
-        for i in df.columns:
-            if i[-1] != '':
-                df.drop([i], axis=1, inplace=True)
-        self.result = df
-    
-        return df
-    def write_excel(self):
-        self.result.to_excel('output.xlsx')
-        
+
 ###################################################################################################################
 from  tkinter import *
 import os
 root = Tk()
 root.title("Программа для получения отчётов от расходомеров")
-root.geometry("600x200")
-asd = None
+root.geometry("500x200")
 value_1 = StringVar()
 value_2 = StringVar()
 
 def show():
+    
     text_1 = value_1.get()
     text_2 = value_2.get()
     print(r2.getboolean())
     
+    
+def soatlik():
+    
+    try:
+        
+        data = pd.read_excel('ExChange.xls')
+        df = pd.DataFrame(data)
+        df['Дата_и_Время'] = pd.to_datetime(df['Дата_и_Время'])
+        df[df.columns[1:]] = df[df.columns[1:]].apply(np.diff, axis = 0)
+        df_copy = np.array(df[df.columns[1:]][:-1])
+        df.drop(index=0, inplace=True)
+        df[df.columns[1:]] = df_copy
 
+        for i in df.columns[1:]:
+            df[i] = np.where((df[i] < 0),  0, df[i])
+
+        df.to_excel('output.xlsx')
+        
+        print(df.head())
+        l_2["text"] = "Часовой отчет сохранен.   Путь к файлу:   " + os.path.abspath('') + '\\'  + 'output.xlsx'
+    
+    except Exception as e:
+        print(
+            type(e).__name__,  # TypeError
+            __file__,  # /tmp/example.py
+            e.__traceback__.tb_lineno )    
+    
     
 def kunlik():
-    a = A('24H')
-    a.raznitsa()
+    
     try:
-        a.write_excel()
+        freq='24H'
+        col_names = []
+        df = pd.DataFrame(pd.read_excel('ExChange.xls'))
+        df['Дата_и_Время'] = pd.to_datetime(df['Дата_и_Время'])
+        df = df.groupby(pd.Grouper(key="Дата_и_Время", freq=freq)).agg(['min', 'max'])
+        for i in range(0,len(df.columns)-1,2):
+            df[df.columns[i][0]+"_result"] =  df[df.columns[i+1]] - df[df.columns[i]]
+            col_names.append(df.columns[i][0])
+        df = df.dropna()
+        for i in df.columns:
+            if i[-1] != '':
+                df.drop([i], axis=1, inplace=True)
+        df.columns = col_names
+        df.to_excel('output.xlsx')     
+    
         l_2["text"] = "Дневной отчет сохранен.   Путь к файлу:   " + os.path.abspath('') + '\\'  + 'output.xlsx'
     except:
         print('Error')
+        
 def oylik():
-    a = A('1M')
-    a.raznitsa()
+    
     try:
-        a.write_excel()
+        freq='1M'
+        col_names = []
+        df = pd.DataFrame(pd.read_excel('ExChange.xls'))
+        df['Дата_и_Время'] = pd.to_datetime(df['Дата_и_Время'])
+        df = df.groupby(pd.Grouper(key="Дата_и_Время", freq=freq)).agg(['min', 'max'])
+        for i in range(0,len(df.columns)-1,2):
+            df[df.columns[i][0]+"_result"] =  df[df.columns[i+1]] - df[df.columns[i]]
+            col_names.append(df.columns[i][0])
+        df = df.dropna()
+        for i in df.columns:
+            if i[-1] != '':
+                df.drop([i], axis=1, inplace=True)
+        df.columns = col_names
+        df.to_excel('output.xlsx')
+        
         l_2["text"] = "Месячный отчет сохранен.   Путь к файлу:   " + os.path.abspath('') + '\\' + 'output.xlsx'
     except:
         print('Error')
@@ -61,11 +92,14 @@ def oylik():
 l=Label(text="Получить отчёт:")
 e1=Entry(textvariable=value_1)
 e2=Entry(textvariable=value_2)
+b_0=Button(command=soatlik, text="Часовой")
 b_1=Button(command=kunlik, text="Дневной")
 b_2=Button(command=oylik, text="Месячный")
 l_2=Label()
 l.pack()
+b_0.pack()
 b_1.pack()
 b_2.pack()
 l_2.pack()
 root.mainloop()
+
